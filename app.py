24a47d0899700e1b81179a89c128a839de4542d6
zangyaku.fighter <zangyaku.fighter@gmail.com>
2025-03-29 23:23:32 +0900

    会話履歴の機構を追加した。

--- app.py
+++ app.py
@@ -2,15 +2,28 @@ import streamlit as st
 
 st.title("langchain-streamlit-app")
 
-prompot = st.chat_input("What is up?")
 # streamlitはファイル単位で上から下まで処理を再実行する
-# print(prompot)
+# １画面の中で画面のリロードをまたいで入力値を引き渡すにはセッション変数stに値を格納する
+if "messages" not in st.session_state: # st.session_state に messagesがない場合
+    st.session_state.messages = [] # st.session_state.messsages を空のリストで初期化
 
-if prompot: # 入力された文字列がある
+for messages in st.session_state.messages: # st.session_state.messagesでループ
+    with st.chat_message(messages["role"]):
+        st.markdown(messages["content"])
+
+prompt = st.chat_input("What is up?")
+
+if prompt: # 入力された文字列がある
     with st.chat_message("user"): #ユーザーのアイコン
-        st.markdown(prompot) # promptをマークダウンとして整形表示
+        st.markdown(prompt) # promptをマークダウンとして整形表示
+
+    # ユーザの入力内容をst.session_state.messsages に追加
+    st.session_state.messages.append({"role":"user", "content":prompt})
 
     with st.chat_message("assistant"): # AIのアイコンで
         response = "こんにちは"
         st.markdown(response)
+    
+    # 応答を st.session_state.messsages に追加
+    st.session_state.messages.append({"role":"assistant", "content":response})
 
